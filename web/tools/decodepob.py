e476a8597c87b38c0a1131a6640f45289acf1d99
afg <afg984@gmail.com>
2019-06-30 03:31:33 +0800

    use clipboard to avoid problems

--- web/tools/decodepob.py
+++ web/tools/decodepob.py
@@ -1,7 +1,8 @@
+import subprocess
 import zlib
 import base64
 
 import lxml.etree
 
 
-print(lxml.etree.tounicode(lxml.etree.fromstring(zlib.decompress(base64.urlsafe_b64decode(input().strip()))), pretty_print=True))
+print(lxml.etree.tounicode(lxml.etree.fromstring(zlib.decompress(base64.urlsafe_b64decode(subprocess.check_output(['xclip', '-clipboard', '-out'])))), pretty_print=True))
