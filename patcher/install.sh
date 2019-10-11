3c67e8a2e157446e38862ad6d1aedf88b09d209c
afg <afg984@gmail.com>
2019-10-11 20:16:47 +0800

    also build poepatcher in install.sh

--- patcher/install.sh
+++ patcher/install.sh
@@ -1,3 +1,4 @@
 python -m venv venv
 venv/bin/pip install git+https://github.com/OmegaK2/PyPoE#egg=PyPoE[cli] tqdm
 venv/bin/pypoe_exporter config set ggpk_path install.sh  # just to suppress warnings
+go build -o patcher/poepatcher patcher/patcher.go
