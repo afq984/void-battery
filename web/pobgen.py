032f27f5dbf8964a322a73521b4d8bdb53ea7b11
spadea96334 <16223709+spadea96334@users.noreply.github.com>
2020-10-09 18:50:58 +0800

    update tree version

--- web/pobgen.py
+++ web/pobgen.py
@@ -49,7 +49,7 @@ def Tree(char, tree):
                 'https://www.pathofexile.com/passive-skill-tree/' + get_encoded_tree(char, tree)
             ),
             E.Sockets(*sockets),
-            treeVersion='3_11'
+            treeVersion='3_12'
         ),
         activeSpec='1'
     )
