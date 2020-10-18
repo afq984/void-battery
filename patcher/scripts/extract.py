77b0ecd7e6a92c04f61be2c2cb613723b6d4c79e
afg <afg984@gmail.com>
2020-10-18 12:11:23 +0800

    disallow missing file

--- patcher/scripts/extract.py
+++ patcher/scripts/extract.py
@@ -1,16 +1,15 @@
 import sys
 import os.path
 from PyPoE.poe.file.file_system import FileSystem
+from PyPoE.poe.file import bundle
 
+if bundle.ooz is None:
+    bundle.ooz = bundle.ffi.dlopen('ooz/build/liblibooz.so')
 if __name__ == '__main__':
     file_system = FileSystem(root_path='Content.ggpk.d/latest')
 
     for filePath in sys.argv[1:]:
-        try:
-            data = file_system.get_file(filePath)
-        except FileNotFoundError:
-            print('%s not exist' % filePath)
-            continue
+        data = file_system.get_file(filePath)
 
         desPath = os.path.join('out/extracted', os.path.basename(filePath))
         file = open(desPath, 'wb')
