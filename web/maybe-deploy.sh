ddf3c18fb16d5737f236f607d7f43095a69274d1
Yu, Li-Yu <afg984@gmail.com>
2020-11-22 06:27:59 +0800

    fix version url

--- web/maybe-deploy.sh
+++ web/maybe-deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-remote_fingerprint="$(curl -s --fail https://void-battery.appspot.com/pob/fingerprint)"
+remote_fingerprint="$(curl -s --fail https://void-battery.afq984.org/pob/fingerprint)"
 echo fingerprints:
 echo "remote: $remote_fingerprint"
 
 local_fingerprint="$(cat nebuloch/data/fingerprint.txt)"
 echo " local: $local_fingerprint"
 
-remote_version="$(curl -s --fail https://void-battery.appspot.com/version)"
+remote_version="$(curl -s --fail https://void-battery.afq984.org/version)"
 echo versions:
 echo "remote: $remote_version"
 
