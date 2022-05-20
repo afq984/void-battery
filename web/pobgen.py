699f7d8b3d8b1aa9bd534134058fadbbabbade4a
Li-Yu Yu <afg984@gmail.com>
2022-05-20 09:26:01 +0800

    Bump tree version to 3_18

--- web/pobgen.py
+++ web/pobgen.py
@@ -66,7 +66,7 @@ def Tree(char, tree):
                 + get_encoded_tree(char, tree)
             ),
             E.Sockets(*sockets),
-            treeVersion='3_17',
+            treeVersion='3_18',
         ),
         activeSpec='1',
     )
