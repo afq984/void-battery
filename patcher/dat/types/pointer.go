91e0cb19a661f7097426bd2cc4b95f9667af586c
Li-Yu Yu <afg984@gmail.com>
2022-12-16 06:40:07 +0800

    Support 3.20
    Switch to dat64

--- patcher/dat/types/pointer.go
+++ patcher/dat/types/pointer.go
@@ -12,11 +12,11 @@ type Pointer struct {
 var _ Scalar = &Pointer{}
 
 func (t *Pointer) Size() int64 {
-	return 4
+	return 8
 }
 
 func (t *Pointer) ValueAt(r *File, off int64) (Value, error) {
-	var ptr int32
+	var ptr int64
 	err := binary.Read(
 		io.NewSectionReader(r, off, t.Size()),
 		binary.LittleEndian,
