0e7f2fbb41540185d4ec0646731364b3c85bdc03
Li-Yu Yu <afg984@gmail.com>
2023-08-25 22:59:50 +0800

    Update tree version

--- web/pobgen.py
+++ web/pobgen.py
@@ -99,7 +99,7 @@ def Tree(char, tree):
                 + get_encoded_tree(char, tree)
             ),
             E.Sockets(*sockets),
-            treeVersion='3_21',
+            treeVersion='3_22',
         ),
         activeSpec='1',
     )
