15de377e04565010456735f493f35b2d7c28e757
Li-Yu Yu <afg984@gmail.com>
2026-01-01 01:51:44 +0900

    Add support for u16, u32

--- patcher/dat/types/int.go
+++ patcher/dat/types/int.go
@@ -27,6 +27,27 @@ func (t *Int16) ValueAt(r *File, off int64) (Value, error) {
 	return IntValue(result), nil
 }
 
+type UInt16 struct{}
+
+var _ Scalar = &UInt16{}
+
+func (t *UInt16) Size() int64 {
+	return 2
+}
+
+func (t *UInt16) ValueAt(r *File, off int64) (Value, error) {
+	var result uint16
+	err := binary.Read(
+		io.NewSectionReader(r, off, int64(t.Size())),
+		binary.LittleEndian,
+		&result,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return IntValue(result), nil
+}
+
 type Int32 struct{}
 
 var _ Scalar = &Int32{}
@@ -37,6 +58,27 @@ func (t *Int32) Size() int64 {
 
 func (t *Int32) ValueAt(r *File, off int64) (Value, error) {
 	var result int32
+	err := binary.Read(
+		io.NewSectionReader(r, off, int64(t.Size())),
+		binary.LittleEndian,
+		&result,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return IntValue(result), nil
+}
+
+type UInt32 struct{}
+
+var _ Scalar = &UInt32{}
+
+func (t *UInt32) Size() int64 {
+	return 4
+}
+
+func (t *UInt32) ValueAt(r *File, off int64) (Value, error) {
+	var result uint32
 	err := binary.Read(
 		io.NewSectionReader(r, off, int64(t.Size())),
 		binary.LittleEndian,
